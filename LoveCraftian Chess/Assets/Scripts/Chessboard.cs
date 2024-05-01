270cb8f0765401774bcf6ee2ced759319443bb92
IMxSCARED <brianmcnamee7503@gmail.com>
2024-05-01 10:31:58 +1000

    aligned created chessboard with prefab variant.

--- LoveCraftian Chess/Assets/Scripts/Chessboard.cs
+++ LoveCraftian Chess/Assets/Scripts/Chessboard.cs
@@ -7,7 +7,9 @@ public class Chessboard : MonoBehaviour
 
     [Header("Art Stuff")]
     [SerializeField] private Material tileMaterial;
-
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
 
     //LOGIC
     private const int TILE_COUNT_X = 8;
@@ -15,19 +17,20 @@ public class Chessboard : MonoBehaviour
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    private Vector3 bounds;
 
 
 
     private void Awake()
     {
-        generateAlltiles(1, 8, 8);
+        generateAlltiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
     private void Update()
     {
         if (!currentCamera)
         {
-            currentCamera = Camera.main;
+            currentCamera = Camera.current;
             return;
         }
         RaycastHit info;
@@ -43,7 +46,7 @@ public class Chessboard : MonoBehaviour
                 currentHover = hitPosition;
                 tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
             }
-            //if we were already hovernig a tile, change prewius
+            //if we were already hovernig a tile, change previous
             if (currentHover != hitPosition)
             {
                 tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
@@ -62,7 +65,13 @@ public class Chessboard : MonoBehaviour
     }
     private void generateAlltiles(float tileSize, int tileCountX, int tileCountY)
     {
-        tiles = new GameObject[tileCountX, tileCountY];
+
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountX / 2) * tileSize) + boardCenter;
+        
+
+
+       tiles =new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
             for (int y = 0; y < tileCountY; y++)
                 tiles[x, y] = GenerateSingleTile(tileSize, x, y);
@@ -79,10 +88,10 @@ public class Chessboard : MonoBehaviour
 
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
-        vertices[1] = new Vector3(x * tileSize, 0,  (y+1) * tileSize);
-        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
-        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset,  (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffset, (y+1) * tileSize) - bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
