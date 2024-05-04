addbd6ba8aef18b57190bd6ae03f05dd67fdc1a6
RussianNinja <stas.katunin@gmail.com>
2024-05-04 16:50:01 +1000

    fixed highlighting issue, Ceated temp colors for black and white team (will be fixed in later tutorial) Had to change camera setting and oriantation to make fit the scene better, not final. 2/5 video watched and coded  pieces now instanciate in into the correct positions.

--- LoveCraftian Chess/Assets/Scripts/Chessboard.cs
+++ LoveCraftian Chess/Assets/Scripts/Chessboard.cs
@@ -11,7 +11,12 @@ public class Chessboard : MonoBehaviour
     [SerializeField] private float yOffset = 0.2f;
     [SerializeField] private Vector3 boardCenter = Vector3.zero;
 
+    [Header("Prefabs && materials")]
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private Material[] teamMaterials;
+
     //LOGIC
+    private ChessPiece[,] chessPieces;
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
@@ -23,47 +28,57 @@ public class Chessboard : MonoBehaviour
 
     private void Awake()
     {
-        generateAlltiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
+        SpawnAllPieces();
+        PositionAllpieces();
     }
-
     private void Update()
     {
         if (!currentCamera)
         {
-            currentCamera = Camera.current;
-            return;
+            currentCamera = Camera.main;
+            Debug.Log("The !current Camera if statement is triggering");
+            //return;
         }
         RaycastHit info;
-        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
+        if (currentCamera != null)
         {
-            //Get the indexes of tile we hit
-            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+            Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
+            {
+                //Get the indexes of tile we hit
+                Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
 
-            //If we are hovering any tile after not hovering any tile
-            if (currentHover == -Vector2Int.one)
-            {
-                currentHover = hitPosition;
-                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+                //If we are hovering any tile after not hovering any tile
+                if (currentHover == -Vector2Int.one)
+                {
+                    currentHover = hitPosition;
+                    tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+                }
+                //if we were already hovernig a tile, change previous
+                if (currentHover != hitPosition)
+                {
+                    tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                    currentHover = hitPosition;
+                    tiles[currentHover.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+                }
             }
-            //if we were already hovernig a tile, change previous
-            if (currentHover != hitPosition)
+            else
             {
-                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
-                currentHover = hitPosition;
-                tiles[currentHover.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+                if (currentHover != -Vector2Int.one)
+                {
+                    tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                    currentHover = -Vector2Int.one;
+                }
             }
         }
-        else
-        {
-            if (currentHover != -Vector2Int.one)
-            {
-                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
-                currentHover = -Vector2Int.one;
-            }
-        }
+        else 
+        { Debug.LogError("currentCamera is null. Check initialization."); }
+        
     }
-    private void generateAlltiles(float tileSize, int tileCountX, int tileCountY)
+
+    // Generate the Board
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
 
         yOffset += transform.position.y;
@@ -76,7 +91,6 @@ public class Chessboard : MonoBehaviour
             for (int y = 0; y < tileCountY; y++)
                 tiles[x, y] = GenerateSingleTile(tileSize, x, y);
     }
-
     private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
@@ -111,7 +125,75 @@ public class Chessboard : MonoBehaviour
 
     }
 
-    //operations
+    //Spawning of the pieces
+    private void SpawnAllPieces()
+    {
+        chessPieces = new ChessPiece[TILE_COUNT_X, TILE_COUNT_Y];
+
+        int whiteteam = 0, blackteam = 1;
+
+        //White Team
+        chessPieces[0, 0] = SpawnSinglePiece(ChessPieceType.Rook, whiteteam);
+        chessPieces[1, 0] = SpawnSinglePiece(ChessPieceType.Knight, whiteteam);
+        chessPieces[2, 0] = SpawnSinglePiece(ChessPieceType.Bishop, whiteteam);
+        chessPieces[3, 0] = SpawnSinglePiece(ChessPieceType.Queen, whiteteam);
+        chessPieces[4, 0] = SpawnSinglePiece(ChessPieceType.King, whiteteam);
+        chessPieces[5, 0] = SpawnSinglePiece(ChessPieceType.Bishop, whiteteam);
+        chessPieces[6, 0] = SpawnSinglePiece(ChessPieceType.Knight, whiteteam);
+        chessPieces[7, 0] = SpawnSinglePiece(ChessPieceType.Rook, whiteteam);
+
+        for (int i = 0; i < TILE_COUNT_X; i++)
+        {
+            chessPieces[i, 1] = SpawnSinglePiece(ChessPieceType.Pawn, whiteteam);
+        }
+
+        //Black Team
+        chessPieces[0, 7] = SpawnSinglePiece(ChessPieceType.Rook, blackteam);
+        chessPieces[1, 7] = SpawnSinglePiece(ChessPieceType.Knight, blackteam);
+        chessPieces[2, 7] = SpawnSinglePiece(ChessPieceType.Bishop, blackteam);
+        chessPieces[3, 7] = SpawnSinglePiece(ChessPieceType.Queen, blackteam);
+        chessPieces[4, 7] = SpawnSinglePiece(ChessPieceType.King, blackteam);
+        chessPieces[5, 7] = SpawnSinglePiece(ChessPieceType.Bishop, blackteam);
+        chessPieces[6, 7] = SpawnSinglePiece(ChessPieceType.Knight, blackteam);
+        chessPieces[7, 7] = SpawnSinglePiece(ChessPieceType.Rook, blackteam);
+
+        for (int i = 0; i < TILE_COUNT_X; i++)
+        {
+            chessPieces[i, 6] = SpawnSinglePiece(ChessPieceType.Pawn, blackteam);
+        }
+    }
+    private ChessPiece SpawnSinglePiece(ChessPieceType type, int team)
+    {
+        ChessPiece cp = Instantiate(prefabs[(int)type - 1], transform).GetComponent<ChessPiece>();
+        cp.type = type;
+        cp.team = team;
+        cp.GetComponent<MeshRenderer>().material = teamMaterials[team];
+        return cp;
+
+    }
+
+    // Positioning
+    private void PositionAllpieces()
+    {
+        for (int x = 0; x < TILE_COUNT_X; x++)
+            for (int y = 0; y < TILE_COUNT_Y; y++)
+                if (chessPieces[x, y] != null)
+                    PositionSinglePiece(x, y, true);
+
+    }
+    private void PositionSinglePiece(int x, int y,bool force = false)
+    {
+        chessPieces[x, y].currentX = x;
+        chessPieces[x, y].currentX = y;
+        chessPieces[x, y].transform.position = GetTileCenter(x,y);
+    }
+
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        return new Vector3(x * tileSize, yOffset, y * tileSize) - bounds + new Vector3(tileSize / 2, 0, tileSize / 2);
+    }
+
+    // Operations
     private Vector2Int LookupTileIndex(GameObject hitInfo)
     {
         for (int x = 0; x < TILE_COUNT_X; x++)
